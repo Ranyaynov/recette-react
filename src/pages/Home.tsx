08a49815a690671447b9a6bafc4db961f3b74540
Ranya.aitgaghou@ynov.com <ranya.aitgaghou@ynov.com>
2026-09-16 09:20:44 +0200

    initialisation projet

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import recipesData from '../data/recipes.json';
+
+function Home() {
+  return (
+    <main>
+      <h1>Ranya Aitgaghou</h1>
+
+      <div>
+        {recipesData.recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <img src={recipe.image} alt={recipe.name} />
+            <h2>{recipe.name}</h2>
+            <p>Préparation : {recipe.prepTimeMinutes} min</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/pages/Recipe.tsx
@@ -0,0 +1,9 @@
+function Recipe() {
+  return (
+    <main>
+      <h1>Détail de la recette</h1>
+    </main>
+  );
+}
+
+export default Recipe;
